86906eb9d09c6f8a6abf7bee9558c721f2860cbe
chadthueson <chadthueson@yahoo.com>
2015-05-23 16:59:46 -0700

    added statistics

---
+++ MIPS/MIPS/Statistics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+#include <stdlib.h>
+
+
+struct statistics {
+
+
+
+	int PC;
+	int total_instruct;
+	int arith_instruct;
+	int logic_instruct;
+	int memory_instruct;
+	int control_instruct;
+};
